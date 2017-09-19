a2b61347d5ac483a90bf2679499381375ca8ce06
smfarzaneh <smf552@nyu.edu>
2017-09-18 23:58:50 -0400

    does second homework problem

--- do.py
+++ do.py
@@ -1,36 +1,43 @@
 # Do-the-homework script
-# run this script to generate all the plots in the report. 
+# run this script to generate all the figures in the report. 
 
 # import packages
 import numpy as np
 import mandelbrot as mb
+import millikan as mk
 
 ## First Problem: Mandelbrot sets
+def doFirstProblem():
+	# set parameters
+	n_grid_low = 40
+	n_grid_high = 2000
 
-# set parameters
-n_grid_low = 40
-n_grid_high = 2000
+	# generate low resolution Mandelbrot grid
+	G = mb.mandelbrotGrid(n_grid_low)
 
-# generate low resolution Mandelbrot grid
-G = mb.mandelbrotGrid(n_grid_low)
+	# plot low resolution grayscale
+	mb.plotMandelbrotGray(G, n_grid_low)
 
-# plot low resolution grayscale plot
-mb.plotMandelbrotGray(G, n_grid_low)
+	# generate high resolution Mandelbrot grid
+	G = mb.mandelbrotGrid(n_grid_high)
 
-# generate high resolution Mandelbrot grid
-G = mb.mandelbrotGrid(n_grid_high)
+	# plot high resolution grayscale
+	mb.plotMandelbrotGray(G, n_grid_high)
 
-# plot high resolution grayscale plot
-mb.plotMandelbrotGray(G, n_grid_high)
+	# plot high resolution colored
+	mb.plotMandelbrotColored(G, n_grid_high)
 
-# plot high resolution colored plot
-mb.plotMandelbrotColored(G, n_grid_high)
-
-# plot high resolution colored plot in log scale
-mb.plotMandelbrotLog(G, n_grid_high)
-
+	# plot high resolution colored in log scale
+	mb.plotMandelbrotLog(G, n_grid_high)
 
 ## Second Problem: Photoelectric effect
+def doSecondProblem():
+	data_matrix = mk.loadData()
+	mk.plotMillikan(data_matrix)
+
+# Do homework problems
+doFirstProblem()
+doSecondProblem()
 
 
 
